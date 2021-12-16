05aff7a85d511a1d7dcc92a9623e4549ec004fff
MrSumrooz <93378338+MrSumrooz@users.noreply.github.com>
2021-12-16 15:41:40 +0100

    html

---
+++ index.js
@@ -0,0 +1,19 @@
+let teamMembers = []
+
+const inputTeam = document.getElementById("TeamInput");
+const TeamCountNode = document.getElementById("numberTeam");
+const TeamDisplayoutput = document.getElementById("teamsOutput");
+const Genratebuttons = document.getElementById("teamsGenerator");
+
+
+
+
+
+let  InputTeamsNames = function() {
+    alert("hi")
+    output.innerHTML = "Hi this is New li";
+    const values = input.value;
+    const total = sums.value;
+    let person = values.split(",");
+
+}
