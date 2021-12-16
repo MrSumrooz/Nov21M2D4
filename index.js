f67a0c90128ee280df641472c9af3496488f3bbc
MrSumrooz <93378338+MrSumrooz@users.noreply.github.com>
2021-12-17 00:05:24 +0100

    changes

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 
 
-const inputTeam = document.getElementById("TeamInput");
 
+const inputFromUser = document.getElementById("TeamInput")
 
+let inputTeamNames = function(){
 
+    let parentUlNode = document.getElementById("Team-List")
+    let li = document.createElement("li")
+    li.classList.add("list-group-item")
+    li.innerHTML = inputFromUser.value 
+    parentUlNode.appendChild(li)
+   
 
+}
 
+const teamNumber = document.getElementById("TeamCount")
+const teamRow = document.getElementById("teamsOutput")
 
-let  InputTeamsNames = function() {
+let createTeams = function(){
     
-    let teamInputFromUser = inputTeam.value
 
-    teamInputFromUser.split(",")
+    for(let i =0; i < teamNumber.value; i++){
+        teamRow.innerHTML += `<div  class="col-2 team">
+                                    <h6>Teams:${i + 1} </h6>
+                                    <ul class="list-group "> 
+                                    
+                                    </ul>
+                                    </div>`
+    }
 
-    return teamInputFromUser
+}
+
+
+
+
+let assign = function(){
+    let teams = document.querySelector(".team")
+    const Person = document.getElementById("Team-List").children[0]
     
 
+    let randomIndex = Math.floor(Math.random() * teams.length)
+
+    teams[randomIndex].appendChild(Person)
+
 
 }
 
-let selectedTeamArray = []
-
-
-let generateMemberFromInput = function(){
-    const selectedTeam = document.getElementById("SelectedTeam");
-    selectedTeam.value = InputTeamsNames()
-
-    selectedTeamArray.push(InputTeamsNames())
-
-    
-}
-
-
-let TeamNumber = function(){
-const TeamCountNode = document.getElementById("numberTeam");
-
-    let TeamnumberNode = 0
-
-    TeamnumberNode = TeamCountNode.value
-
-return TeamnumberNode
-
-}
-
-let TeamCreator = function(){
-
-}
 
 
